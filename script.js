ce3afe6611a18317dc881253d1c559753ae81f0d
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-01-21 03:59:41 -0500

    taking input and console

---
+++ script.js
@@ -0,0 +1,8 @@
+var inputElement = document.getElementById('input-box')
+
+var buttonEvent = document.getElementById('btn-translate');
+
+buttonEvent.addEventListener('click', function log(){
+    console.log(inputElement.value);
+    console.log('mohan')    
+});
