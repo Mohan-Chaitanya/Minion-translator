e72a1f407a115e4b9ac698449c7d3f7ec45ef241
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-01-21 08:13:40 -0500

    Final minion App

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-var inputElement = document.getElementById('input-box')
-
-var buttonEvent = document.getElementById('btn-translate');
-
-buttonEvent.addEventListener('click', function clickHandler(){
-     outputData.innerHTML = 'minionished: ' + inputElement.value;
-});
-
+var inputElement = document.querySelector('#input-box')
+var buttonEvent = document.querySelector('#btn-translate');
 var outputData = document.querySelector('#output');
-//console.log(outputData);
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translateURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Duh!! there is an error" + error);
+}
+
+function clickHandler (){
+    var inputText = inputElement.value;
+
+    fetch(translateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            outputData.innerHTML = json.contents.translated;
+        })
+        .catch(errorHandler);
+};
+
+buttonEvent.addEventListener('click', clickHandler);
