3740406f31e9dd626e66cf28f5e942c79bc77f7c
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-01-21 04:52:51 -0500

    input output processing

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var inputElement = document.getElementById('input-box')
 
 var buttonEvent = document.getElementById('btn-translate');
 
-buttonEvent.addEventListener('click', function log(){
-    console.log(inputElement.value);
-    console.log('mohan')    
+buttonEvent.addEventListener('click', function clickHandler(){
+     outputData.innerHTML = 'minionished: ' + inputElement.value;
 });
+
+var outputData = document.querySelector('#output');
+//console.log(outputData);
